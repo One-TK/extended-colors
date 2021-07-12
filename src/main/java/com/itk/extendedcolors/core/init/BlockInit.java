a9600622d56d7e27d827483559892dad27d8f439
One-TK <87282041+One-TK@users.noreply.github.com>
2021-07-11 22:31:25 -0500

    Added 8 more colors

--- src/main/java/com/itk/extendedcolors/core/init/BlockInit.java
+++ src/main/java/com/itk/extendedcolors/core/init/BlockInit.java
@@ -27,4 +27,38 @@ public class BlockInit {
 			() -> new Block(AbstractBlock.Properties.of(Material.WOOL, MaterialColor.COLOR_GRAY).strength(0.8f, 1.2f)
 					.harvestLevel(0).sound(SoundType.WOOL)));
 	
+	public static final RegistryObject<Block> SCARLETWOOLBLOCK = BLOCKS.register("scarletwoolblock",
+			() -> new Block(AbstractBlock.Properties.of(Material.WOOL, MaterialColor.COLOR_GRAY).strength(0.8f, 1.2f)
+					.harvestLevel(0).sound(SoundType.WOOL)));
+	
+	public static final RegistryObject<Block> BRICKREDWOOLBLOCK = BLOCKS.register("brickredwoolblock",
+			() -> new Block(AbstractBlock.Properties.of(Material.WOOL, MaterialColor.COLOR_GRAY).strength(0.8f, 1.2f)
+					.harvestLevel(0).sound(SoundType.WOOL)));
+	
+	public static final RegistryObject<Block> CHESTNUTWOOLBLOCK = BLOCKS.register("chestnutwoolblock",
+			() -> new Block(AbstractBlock.Properties.of(Material.WOOL, MaterialColor.COLOR_GRAY).strength(0.8f, 1.2f)
+					.harvestLevel(0).sound(SoundType.WOOL)));
+	
+	public static final RegistryObject<Block> ORANGEREDWOOLBLOCK = BLOCKS.register("orangeredwoolblock",
+			() -> new Block(AbstractBlock.Properties.of(Material.WOOL, MaterialColor.COLOR_GRAY).strength(0.8f, 1.2f)
+					.harvestLevel(0).sound(SoundType.WOOL)));
+	
+	public static final RegistryObject<Block> SUNSETORANGEWOOLBLOCK = BLOCKS.register("sunsetorangewoolblock",
+			() -> new Block(AbstractBlock.Properties.of(Material.WOOL, MaterialColor.COLOR_GRAY).strength(0.8f, 1.2f)
+					.harvestLevel(0).sound(SoundType.WOOL)));
+	
+	public static final RegistryObject<Block> BITTERSWEETWOOLBLOCK = BLOCKS.register("bittersweetwoolblock",
+			() -> new Block(AbstractBlock.Properties.of(Material.WOOL, MaterialColor.COLOR_GRAY).strength(0.8f, 1.2f)
+					.harvestLevel(0).sound(SoundType.WOOL)));
+	
+	public static final RegistryObject<Block> VIVIDTANGERINEWOOLBLOCK = BLOCKS.register("vividtangerinewoolblock",
+			() -> new Block(AbstractBlock.Properties.of(Material.WOOL, MaterialColor.COLOR_GRAY).strength(0.8f, 1.2f)
+					.harvestLevel(0).sound(SoundType.WOOL)));
+	
+	public static final RegistryObject<Block> BURNTORANGEWOOLBLOCK = BLOCKS.register("burntorangewoolblock",
+			() -> new Block(AbstractBlock.Properties.of(Material.WOOL, MaterialColor.COLOR_GRAY).strength(0.8f, 1.2f)
+					.harvestLevel(0).sound(SoundType.WOOL)));
+	
+	
+	
 }
